a4f8a51710bf0cf8533dd5f8240c184a9395590a
rbs2rbs <renan.bisposilva@gmail.com>
2019-02-21 19:12:30 -0300

    Sobe site completo

--- glmr/settings.py
+++ glmr/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from credenciais import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -41,12 +42,13 @@ INSTALLED_APPS = [
     'blog'
 ]
 
-EMAIL_BACKEND =  'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'renan.bisposilva@gmail.com'
-EMAIL_HOST_PASSWORD = 'bdxjdaktciatokxl'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
+EMAIL_BACKEND =  EMAIL_BACKEND
+EMAIL_HOST = EMAIL_HOST
+EMAIL_HOST_USER = EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
+EMAIL_PORT = EMAIL_PORT
+EMAIL_USE_SSL = EMAIL_USE_SSL
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
