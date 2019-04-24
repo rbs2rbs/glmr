1a2d7551564fd5aee21f859c61420fc9dabac451
rbs2rbs <renan.bisposilva@gmail.com>
2019-04-23 22:29:15 -0300

    Cria method de autenticação para gerar api do mendeley

--- academico/urls.py
+++ academico/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from academico import views
 
 urlpatterns = [
-    url('',views.academico, name = "academico")
+    # url('',views.academico, name = "academico"),
+    url(r'^gettoken/$',views.gettoken, name = "gettoken"),
+    url(r'^token/$',views.token, name = "token"),
+    url(r'^token/busca',views.academico, name = "busca"),
 ]
--- academico/views.py
+++ academico/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from academico.scholar import *
 from urllib.parse import urlparse
+from django.http import HttpResponse
 
 
 import json
@@ -9,54 +10,74 @@ from random import randint
 from django.views.generic import TemplateView
 # Create your views here.
 
+def gettoken(request):
+    if request.method == "GET":
+        return render(request,'gettoken.html')
+
+
+def token(request):
+    
+    if request.method == "POST":
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        request.session['token'] = body
+        return HttpResponse(
+            json.dumps("OK"),
+            content_type= 'application/json',
+            status= 200
+        )
+    return render(request,'token.html')
+
+
+
+
 def academico (request):
     if request.method == 'GET':
-        print(request.session)
-        # print(urlparse(request.build_absolute_uri))
-        if(request.GET.get('access_token') is not None):
-            busca = {
-                'busca' : request.GET.get('busca'),
-                'tipo' : request.GET.get('tipo'),
-                'token' : request.GET.get('token'),
-                'titulos' : [],
-                'p_chave' : {
-                    'valor' : [],
-                    'label' : [],
-                    },
-                'anos': {
-                    'valor' : [],
-                    'ano' : []
-                    }
+        busca = {
+            'busca' : request.GET.get('busca'),
+            'tipo' : request.GET.get('tipo'),
+            'token' : request.session['token'],
+            'titulos' : [],
+            'p_chave' : {
+                'valor' : [],
+                'label' : [],
+                },
+            'anos': {
+                'valor' : [],
+                'ano' : []
                 }
+            }
+        
+        if (busca['busca']!="" and busca['busca']!= None and busca['tipo']!="" and busca['tipo']!= None):
+            dados = Scholar(
+                busca = busca['busca'], 
+                tipo = busca['tipo'], 
+                token = busca['token']
+            )
+        
+            dados.gerar()
             
-            if (busca['busca']!="" and busca['busca']!= None and busca['tipo']!="" and busca['tipo']!= None):
-                dados = Scholar(
-                    busca = busca['busca'], 
-                    tipo = busca['tipo'], 
-                    token = busca['token']
-                )
+            p_chaves = grafico('palavras-chave',dados)
             
-                dados.gerar()
-                
-                p_chaves = grafico('palavras-chave',dados)
-                
-                for index,p_chave in enumerate(p_chaves):
-                    busca['p_chave']['valor'].append(p_chave[1])
-                    busca['p_chave']['label'].append(p_chave[0])
+            for index,p_chave in enumerate(p_chaves):
+                busca['p_chave']['valor'].append(p_chave[1])
+                busca['p_chave']['label'].append(p_chave[0])
 
-                    if (index == 9): break
-                
-                busca['p_chave']['label'] = json.dumps(busca['p_chave']['label'])
-                busca['p_chave']['valor'] = json.dumps(busca['p_chave']['valor'])
-                
-                for titulo in dados.saida:
-                    busca['titulos'].append(titulo['titulo'])
+                if (index == 9): break
+            
+            busca['p_chave']['label'] = json.dumps(busca['p_chave']['label'])
+            busca['p_chave']['valor'] = json.dumps(busca['p_chave']['valor'])
+            
+            for titulo in dados.saida:
+                busca['titulos'].append(titulo['titulo'])
+
+
+            for an,v in grafico('ano',dados).items():
+                busca['anos']['valor'].append(int(v))
+                busca['anos']['ano'].append(int(an))
+
+        return render(request,'academico.html', busca)
+    else:
+        return render(request,'token.html')
     
 
-                for an,v in grafico('ano',dados).items():
-                    busca['anos']['valor'].append(int(v))
-                    busca['anos']['ano'].append(int(an))
-
-            return render(request,'academico.html', busca)
-        else:
-            return render(request,'token.html')
